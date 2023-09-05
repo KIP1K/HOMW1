c88b8c70c0e9c85ec9a72c68290b2bf8d2aa66ff
kip1k <motafaka@yandex.ru>
2023-09-05 14:24:29 +0300

    millie

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 13676 ; //стоимость билета
+        int y = 20 ; //бонусная миля
+        int z = x/y ;
+        String Q = "Милей на вашем счёте: ";
+        String QQ = Q+z;
+        System.out.println(QQ) ;
+    }
+}
